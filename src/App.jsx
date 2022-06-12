0feadfa5ad844c08134a28e8365ceb840d7b9e02
khangle <khanglmph14697@gmail.com>
2022-06-13 05:57:04 +0700

    Feat: navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>hello word</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="header__navbar">
+      <div className="container">
+        <a
+          href=""
+          className="header__navbar--logo"
+        >
+          Nur<span>ture.</span>
+        </a>
+        <nav className="header__navbar--menu">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Courses</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Reviews</a>
+            </li>
+          </ul>
+        </nav>
+        <a href="#" className="btn">
+          Sign Up
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
